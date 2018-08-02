1a7945abfc3c24e13c84f58204f29e0595eb47c0
duck84 <mmcgrath@pdx.edu>
2018-08-02 15:32:30 -0700

    Hashing all of Hamlet

--- reader/src/main.rs
+++ reader/src/main.rs
@@ -2,10 +2,11 @@ use std::io::Read;
 use std::error::Error;
 use std::path::Path;
 use std::fs::File;
+use std::collections::HashMap;
 
 
 fn main() {
-    let path = Path::new("../../text/hamlet.txt");
+    let path = Path::new("../../text/test.txt");
     let display = path.display();
 
     let mut file = match File::open(&path){
@@ -18,4 +19,24 @@ fn main() {
         Err(why) => panic!("could not open {}: {}", display, why.description()),
         Ok(_) => print!("{} contains:\n{}", display, text),
     }
+
+    let tokens: Vec<&str> = text.split_whitespace().collect();
+
+    let group = tokens.windows(3);
+
+    let mut histogram: HashMap<&str, (&str, &str)> = HashMap::new();
+
+    for words in group {
+        
+        let prefix = words[0];
+        let suffix = (words[1], words[2]);
+        histogram.insert(prefix, suffix);
+    }
+
+    for(key, value) in histogram{
+        println!("{} / {:?}", key, value)
+    }
+
+
+
 }
